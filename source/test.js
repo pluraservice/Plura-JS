1f4ab8990238a816395dcb5058503c129af78191
pluraservice <163671843+pluraservice@users.noreply.github.com>
2024-03-17 17:23:04 +0100

    Delete source directory

--- source/test.js
+++
@@ -1,8 +0,0 @@
-import { PluraJS } from "./dist/PluraJS.cjs.js";
-const browserName = PluraJS.Browser.getBrowserName();
-
-if (browserName === "Chrome") {
-    PluraJS.WBFunctions.newLog(`Browser Name: ${browserName}`, "color: blue;", true); // Browser Name: Chrome
-} else {
-    PluraJS.WBFunctions.closePage();
-}
